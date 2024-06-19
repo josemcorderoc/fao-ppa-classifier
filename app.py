18c4a44743fae246eff1f9369374d199947abfba
José Miguel Cordero Carvacho <josemcorderoc@gmail.com>
2024-06-19 20:18:06 +0100

    feat: display top three labels (by probability), implement multi output feedback

--- app.py
+++ app.py
@@ -1,115 +1,120 @@
-from datetime import datetime
+from models.feedback import Feedback
+from models.embeddings.openai_embedding import OpenAIEmbedding
+from interfaces.repository import Repository
+from interfaces.embedding import Embedding
+from interfaces.classifier import Classifier
+from infrastructure.s3_repository import S3Repository
 import os
 import pickle
 import sys
+from datetime import datetime
 from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parent / "src"))
+
 import pandas as pd
 import pytz
 import streamlit as st
 import yaml
 from streamlit import session_state as ss
-from infrastructure.s3_repository import S3Repository
-from interfaces.classifier import Classifier
-from interfaces.embedding import Embedding
-from interfaces.repository import Repository
-from models.feedback import Feedback
-from models.embeddings.openai_embedding import OpenAIEmbedding
-#from src import classifiers
-# from streamlit_extras.stateful_button import button 
+
+sys.path.append(str(Path(__file__).resolve().parent / "src"))
 
 with open("config.yaml", "r") as file:
     config = yaml.safe_load(file)
-    
+
     for key in config:
         env_var = os.getenv(key)
         if env_var is not None:
             config[key] = env_var
 
-#classifier = getattr(classifiers, config['classifier_function_name'])
-# embeddings_model = getattr(embeddings, config['embeddings_model_class'])()
+
 ppas = ['BE.1',
- 'BE.2',
- 'BE.3',
- 'BE.4',
- 'BL.1',
- 'BL.2',
- 'BL.3',
- 'BL.4',
- 'BL.5',
- 'BL.6',
- 'BN.1',
- 'BN.2',
- 'BN.3',
- 'BN.4',
- 'BN.5',
- 'BP.1',
- 'BP.2',
- 'BP.3',
- 'BP.4',
- 'BP.5']
+        'BE.2',
+        'BE.3',
+        'BE.4',
+        'BL.1',
+        'BL.2',
+        'BL.3',
+        'BL.4',
+        'BL.5',
+        'BL.6',
+        'BN.1',
+        'BN.2',
+        'BN.3',
+        'BN.4',
+        'BN.5',
+        'BP.1',
+        'BP.2',
+        'BP.3',
+        'BP.4',
+        'BP.5']
 
 
-for attribute in ["predicted_classification", "selected_classification", "liked", "disliked", "user_input", "classified_text"]:
+for attribute in ["predicted_classification", "liked", "disliked", "user_input", "classified_text"]:
     if attribute not in ss:
         ss[attribute] = None
 
+for attribute in ["selected_missing_ppas", "selected_incorrect_ppas"]:
+    if attribute not in ss:
+        ss[attribute] = []
 
 
-def main(repo: Repository, embedding: Embedding, clf: Classifier): 
-    
+def main(repo: Repository, embedding: Embedding, clf: Classifier):
+
     def run_classification(prompt_input: str):
         text_embedding = embedding.generate(prompt_input)
-        # similarities = classifier(text_embedding)
-        # probabilities = pd.Series(softmax(similarities + 1), index=similarities.index)
-        # classification = probabilities.idxmax()
-        classification = pd.DataFrame(clf.predict_proba(text_embedding.reshape(1, -1)), columns=ppas).iloc[0].idxmax()
-        ss.predicted_classification = classification
-        ss.selected_classification = None
+        classification_result = pd.DataFrame(clf.predict_proba(
+            text_embedding.reshape(1, -1)), columns=ppas).iloc[0]
+        classification_result_top3 = classification_result.sort_values(
+            ascending=False).head(3).index.to_list()
+        ss.predicted_classification = classification_result_top3
         ss.classified_text = prompt_input
-        
+
     st.set_page_config(
         page_title=config['title'],
         page_icon="./data/media/img/favicon.ico",
     )
     st.title(config['title'])
     st.write(config['description'])
-    
+
     user_input = st.text_area(config['text_area_label'], key="user_input")
-    
+
     if ss.user_input:
-        classify_button = st.button(config['button_label'], on_click=run_classification, key='classification', args=(user_input,))
+        classify_button = st.button(
+            config['button_label'], on_click=run_classification, key='classification', args=(user_input,))
 
     if ss.predicted_classification and ss.user_input == ss.classified_text:
-        col1, col2, col3, _ = st.columns([.2, .08, .08, .64  ])
+        col1, col2, col3, _ = st.columns([.2, .08, .08, .64])
         with col1:
-            st.write(f"Classification: {ss.predicted_classification}")
-        
+            st.write(
+                f"Classification: {', '.join(ss.predicted_classification)}")
+
         with col2:
             st.button("👍", key="liked")
-            
+
         with col3:
             st.button("👎", key="disliked")
-            
-    
-    if ss.disliked or ss.selected_classification:
-        st.selectbox(
-            "Select the correct classification:",
-            ppas,
-            index=None, key='selected_classification')
-        
-    if ss.selected_classification:
+
+    if ss.predicted_classification and (ss.liked or ss.disliked or ss.selected_incorrect_ppas or ss.selected_missing_ppas):
+        selected_incorrect_ppas = st.multiselect(
+            "What PPAs are incorrect? ✘",
+            ss.predicted_classification,
+            key="selected_incorrect_ppas"
+        )
+
+        selected_missing_ppas = st.multiselect(
+            "What PPAs are missing? 🔎",
+            [ppa for ppa in ppas if ppa not in ss.predicted_classification],
+            key="selected_missing_ppas"
+        )
+
+    if ss.selected_incorrect_ppas or ss.selected_missing_ppas:
         if st.button("Save"):
-            feedback = Feedback(datetime.now(pytz.utc), user_input, ss.predicted_classification, ss.selected_classification, False, True)
+            feedback = Feedback(datetime.now(pytz.utc), user_input, ss.predicted_classification,
+                                ss.selected_incorrect_ppas, ss.selected_missing_ppas, ss.liked, ss.disliked)
             repo.AddFeedback(feedback)
             st.info("Feedback saved successfully.")
             st.button("Clear All", key="reset", on_click=ss.clear)
-            
-    if ss.liked:
-        feedback = Feedback(datetime.now(pytz.utc), user_input, ss.predicted_classification, ss.predicted_classification, True, False)
-        repo.AddFeedback(feedback)
-        st.info("Feedback saved successfully.")
-        st.button("Clear All", key="reset", on_click=ss.clear)
+
 
 if __name__ == "__main__":
     s3repo = S3Repository()
--- src/models/feedback.py
+++ src/models/feedback.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
 class Feedback:
-    def __init__(self, timestamp: datetime, text: str, predicted_label: str, expected_label: str, liked: bool, disliked: bool):
+    def __init__(self, timestamp: datetime, text: str, predicted_label: str, selected_incorrect_ppas: list[str], selected_missing_ppas: list[str], liked: bool, disliked: bool):
         self.timestamp = timestamp
         self.text = text
         self.predicted_label = predicted_label
-        self.expected_label = expected_label
+        self.selected_incorrect_ppas = selected_incorrect_ppas
+        self.selected_missing_ppas = selected_missing_ppas
         self.liked = liked
         self.disliked = disliked
